0ff71c589e16afac9bc9a457fedf7f4ae4719a1d
elliot-siegel <elliotsiegel@brandeis.edu>
2020-04-25 16:25:35 -0400

    Basic player class and human player subclass

--- src/final_project/HumanPlayer.java
+++ src/final_project/HumanPlayer.java
@@ -1,5 +1,24 @@
 package final_project;
 
-public class HumanPlayer extends Player {
+import java.util.Scanner;
 
+public class HumanPlayer extends Player {
+	
+	public HumanPlayer() {
+		super();
+	}
+
+	@Override
+	public void nextMove() {
+		System.out.print("Direction to move in: ");
+		Scanner in = new Scanner(System.in);
+		String direction = in.next();
+		
+		//this possibly closes all System.in scanners, so it could cause issues
+		in.close();
+		System.out.println();
+		
+		move(direction);
+		
+	}
 }
